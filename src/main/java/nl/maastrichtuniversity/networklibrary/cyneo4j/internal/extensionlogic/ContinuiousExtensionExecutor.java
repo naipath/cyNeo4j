c057974e070b423cf2c5c6c81ba100d708589da1
tvs <teije-vs@hotmail.com>
2017-05-26 16:02:58 +0200

    Formatted interface

--- src/main/java/nl/maastrichtuniversity/networklibrary/cyneo4j/internal/extensionlogic/ContinuiousExtensionExecutor.java
+++ src/main/java/nl/maastrichtuniversity/networklibrary/cyneo4j/internal/extensionlogic/ContinuiousExtensionExecutor.java
@@ -2,10 +2,10 @@ package nl.maastrichtuniversity.networklibrary.cyneo4j.internal.extensionlogic;
 
 /**
  * Allows the developer to create ExtensionExecutors that loop.
- * @author gsu
  *
+ * @author gsu
  */
 public interface ContinuiousExtensionExecutor extends ExtensionExecutor {
 
-	boolean doContinue();
+    boolean doContinue();
 }
